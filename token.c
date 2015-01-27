a91605539921767fd8c05d61ac8410ede8f47bb2
Suratchanan Kraidech <benchbb01@hotmail.com>
2015-01-27 22:54:22 +0700

    Version1:Single-threaded correct output on tiny files

--- token.c
+++ token.c
@@ -9,7 +9,7 @@ struct word_node{
 struct keyval{
 	gchar *str;
 	GSList *head;
-	guint count;
+	//guint count;
 };
 
 //Compare function use for sorting
@@ -24,14 +24,18 @@ gint cmp_word_node(const struct word_node *a,const struct word_node *b){
 	else return g_strcmp0((*a).doc_id,(*b).doc_id);	
 }
 
-void showlinklist(gchar *str,gint *user_data){
-	g_printf(":%s",str);
-}
+/*
 void showtable(guint *hashkey,struct keyval *val,gint *etc){
-	g_printf("%s:%d",(*val).str,(*val).count);
-	g_slist_foreach((*val).head,(GFunc)showlinklist,NULL);
+	GSList *pt;
+	g_printf("%s:%d:",(*val).str,(*val).count);
+	pt = (*val).head;
+	while(pt != NULL){
+		g_printf("%s",(*pt).data);
+		if((pt = g_slist_next(pt)) != NULL)
+			g_printf(",");	
+	} 
 	g_printf("\n");
-}
+}*/
 
 int main(int argc,char **argv){
 FILE *f;
@@ -46,36 +50,36 @@ GDir *dir = g_dir_open(argv[1],0,NULL);
 g_chdir(argv[1]);
 
 while((filename = GINT_TO_POINTER(g_dir_read_name(dir))) !=NULL){
-f = fopen(filename,"r");
-g_printf("[%s]\n",filename);
-	while((ch = fgetc(f))!=EOF){
-		printf("%c",ch);
-		if(g_ascii_isalpha(ch) && j == 0){
-			temp = g_string_new(NULL);
-			g_string_append_c(temp,ch);
-			j++;	
-		}
-		else if(g_ascii_isalpha(ch))
-			g_string_append_c(temp,ch);
-		else if(!g_ascii_isalpha(ch) && j > 0){
-			temp = g_string_ascii_down(temp);
-			node.word = temp;
-			number = g_strsplit_set(filename,".txt",-1);
-			node.doc_id = number[0]+(sizeof(gchar)*4);
-			g_array_append_val(wordlist,node);
-			j=0;		
-		}
-		else j=0;
-	}	
-if(j > 0){
-	temp = g_string_ascii_down(temp);
-	node.word = temp;
-	number = g_strsplit_set(filename,".txt",-1);
-	node.doc_id = number[0] + (sizeof(gchar)*4);
-	g_array_append_val(wordlist,node);
-	j=0;
+	f = fopen(filename,"r");
+	g_printf("[%s]\n",filename);
+		while((ch = fgetc(f))!=EOF){
+			printf("%c",ch);
+			if(g_ascii_isalpha(ch) && j == 0){
+				temp = g_string_new(NULL);
+				g_string_append_c(temp,ch);
+				j++;	
+			}
+			else if(g_ascii_isalpha(ch))
+				g_string_append_c(temp,ch);
+			else if(!g_ascii_isalpha(ch) && j > 0){
+				temp = g_string_ascii_down(temp);
+				node.word = temp;
+				number = g_strsplit_set(filename,".txt",-1);
+				node.doc_id = number[0]+(sizeof(gchar)*4);
+				g_array_append_val(wordlist,node);
+				j=0;		
+			}
+			else j=0;
+		}	
+	if(j > 0){
+		temp = g_string_ascii_down(temp);
+		node.word = temp;
+		number = g_strsplit_set(filename,".txt",-1);
+		node.doc_id = number[0] + (sizeof(gchar)*4);
+		g_array_append_val(wordlist,node);
+		j=0;
 }
-fclose(f);
+	fclose(f);
 }
 
 
@@ -95,6 +99,7 @@ g_printf("%s\t%s\n",(*watch).str,node.doc_id);
 }
 
 guint *hashkey;
+GArray *hashlist = g_array_new(TRUE,FALSE,sizeof(guint));
 struct keyval *val = NULL;
 GHashTable *table = g_hash_table_new((GHashFunc)g_str_hash,(GEqualFunc)g_str_equal);
 for(ind = 0;ind < (*wordlist).len;ind++){
@@ -108,29 +113,44 @@ for(ind = 0;ind < (*wordlist).len;ind++){
 		if(!g_strcmp0((*watch).str,(*val).str))
 		goto warp;
 		}
+	g_array_append_val(hashlist,hashkey);
 	val = g_new(struct keyval,1);
 	(*val).str = (*watch).str;
 	(*val).head = NULL;
 	(*val).head = g_slist_append((*val).head,node.doc_id);
-	(*val).count = 1;
+	//(*val).count = 1;
 	g_hash_table_insert(table,hashkey,val);	
 	}
 	else{
 		//Update value only since it dynamically allocated
 		warp:
 		if(g_slist_find_custom((*val).head,node.doc_id,(GCompareFunc)g_strcmp0)==NULL){
-			((*val).count)++;
+			//((*val).count)++;
 			(*val).head = g_slist_append((*val).head,node.doc_id);
 		}
 	    }
 }
 //Show table data
 g_printf("Show data result:\n");
-g_printf("Number of word:%d\n",g_hash_table_size(table));
-g_hash_table_foreach(table,(GHFunc)showtable,NULL);
 
-//for(ind = 0;ind < (*wordlist).len;ind++)
-
+//Write data result to output
+g_chdir("..");
+f = fopen("outputtemp1","w");
+g_fprintf(f,"%d\n",g_hash_table_size(table));
+for(ind = 0;ind < (*hashlist).len;ind++){
+	GSList *pt;
+	val = g_hash_table_lookup(table,GUINT_TO_POINTER(g_array_index(hashlist,guint,ind)));
+	g_fprintf(f,"%s:%d:",(*val).str,g_slist_length((*val).head));
+	pt = (*val).head;
+	while(pt != NULL){
+		g_fprintf(f,"%s",(*pt).data);
+		if((pt = g_slist_next(pt)) != NULL)
+			g_fprintf(f,",");
+		
+	}
+	g_fprintf(f,"\n");
+}
+fclose(f);
 
 
 
@@ -139,9 +159,9 @@ g_hash_table_foreach(table,(GHFunc)showtable,NULL);
 
 //Data deletion
 for(ind = 0;ind < (*wordlist).len;ind++){
-node = g_array_index(wordlist,struct word_node,ind);
-watch = node.word;
-g_string_free(watch,TRUE);
+	node = g_array_index(wordlist,struct word_node,ind);
+	watch = node.word;
+	g_string_free(watch,TRUE);
 }
 //Finally remove array
 g_array_free(wordlist,TRUE);
