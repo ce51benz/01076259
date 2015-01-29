0378196ffa79eaa160bcb0bb89e3bc75f31d6ba8
Suratchanan Kraidech <benchbb01@hotmail.com>
2015-01-29 21:13:11 +0700

    Revert to use implementation of hashtable-slist with all correct result on single-threaded

--- token.c
+++ token.c
@@ -7,23 +7,21 @@ struct word_node{
 struct keyval{
 	GSList *head;
 };
+
+//Compare function use for sorting
+gint cmp_word_node(guint *a,guint *b){
+	//guint aa,bb;
+	gchar *aaa,*bbb;
+	//aa = *a;
+	//bb = *b;
+	aaa = GUINT_TO_POINTER(*a);
+	bbb = GUINT_TO_POINTER(*b);
+	return g_strcmp0(aaa,bbb);
+}
+
 gint cmp_int(gpointer a,gpointer b){
-return atol(a) - atol(b);
+	return atol(a) - atol(b);
 }
-
-gboolean treetofile(gchar *key,struct keyval *val,FILE *f){
-GSList *pt;
-g_fprintf(f,"%s:%d:",key,g_slist_length(val->head));
-pt = g_slist_sort(val->head,(GCompareFunc)cmp_int);
-while(pt != NULL){
-	g_fprintf(f,"%s",pt->data);
-	pt = g_slist_next(pt);
-	if(pt != NULL)g_fprintf(f,",");
-}
-g_fprintf(f,"\n");
-return FALSE;
-}
-
 int main(int argc,char **argv){
 GSList *listpt = NULL;
 FILE *f;
@@ -32,9 +30,10 @@ gchar *filename,*watch;
 gchar **number;
 GString *temp;
 struct keyval *val;
-GTree *tree = g_tree_new((GCompareFunc)g_strcmp0);
+GHashTable *table = g_hash_table_new((GHashFunc)g_str_hash,(GEqualFunc)g_str_equal);
 gint j=0,ind;
 struct word_node node;
+GArray *wordlist = g_array_new(TRUE,FALSE,sizeof(guint));
 GDir *dir = g_dir_open(argv[1],0,NULL);
 g_chdir(argv[1]);
 temp = g_string_new(NULL);
@@ -50,12 +49,14 @@ while((filename = GINT_TO_POINTER(g_dir_read_name(dir))) !=NULL){
 			else if(!g_ascii_isalpha(ch) && j > 0){
 				temp = g_string_ascii_down(temp);
 				number = g_strsplit_set(filename,".txt",-1);
-				if(!(val = g_tree_lookup(tree,temp->str))){
+				if(!(val = g_hash_table_lookup(table,temp->str))){
 					val = g_new(struct keyval,1);
 					node.word = g_strdup(temp->str);
+
+					g_array_append_val(wordlist,node.word);
 					node.doc_id = g_strdup(number[0] + (sizeof(gchar)*4));
 					val->head = g_slist_append(listpt,node.doc_id);
-					g_tree_insert(tree,node.word,val); 
+					g_hash_table_insert(table,node.word,val); 
 				}
 				else{
 					if(!g_slist_find_custom(val->head,(number[0]+(sizeof(gchar)*4)),(GCompareFunc)g_strcmp0)){
@@ -68,18 +69,18 @@ while((filename = GINT_TO_POINTER(g_dir_read_name(dir))) !=NULL){
 				j=0;
 				g_strfreev(number);
 			}
-			else j=0;
 		}	
 
 	if(j > 0){
 				temp = g_string_ascii_down(temp);
 				number = g_strsplit_set(filename,".txt",-1);
-				if(!(val = g_tree_lookup(tree,temp->str))){
+				if(!(val = g_hash_table_lookup(table,temp->str))){
 					val = g_new(struct keyval,1);
 					node.word = g_strdup(temp->str);
+					g_array_append_val(wordlist,node.word);
 					node.doc_id = g_strdup(number[0] + (sizeof(gchar)*4));
 					val->head = g_slist_append(listpt,node.doc_id);
-					g_tree_insert(tree,node.word,val); 
+					g_hash_table_insert(table,node.word,val); 
 
 				}
 				else{
@@ -94,16 +95,7 @@ while((filename = GINT_TO_POINTER(g_dir_read_name(dir))) !=NULL){
 	fclose(f);
 }
 
-//g_array_sort(wordlist,(GCompareFunc) cmp_word_node);
-
-/*
-g_printf("After sort:\n");
-for(ind =0;ind < (*wordlist).len;ind++){
-node = g_array_index(wordlist,struct word_node,ind);
-watch = node.word;
-g_printf("%s\t%s\n",watch,node.doc_id);
-}
-*/
+g_array_sort(wordlist,(GCompareFunc) cmp_word_node);
 
 
 //Show table data
@@ -112,40 +104,25 @@ g_printf("Show data result:\n");
 //Write data result to output
 g_chdir("..");
 f = fopen("outputtemp1","w");
-g_fprintf(f,"%d\n",g_tree_nnodes(tree));
-
-g_tree_foreach(tree,(GTraverseFunc) treetofile,f);
-
-
-/*
-for(ind = 0;ind < (*wordlist).len;ind++){
-	GSList *pt;
+g_fprintf(f,"%d\n",wordlist->len);
+for(ind = 0;ind < wordlist->len;ind++){
 	watch = GUINT_TO_POINTER(g_array_index(wordlist,guint,ind));
 	val = g_hash_table_lookup(table,watch);
-	g_fprintf(f,"%s:%d:",watch,g_slist_length((*val).head));
-	pt = g_slist_sort(val->head,(GCompareFunc)cmp_int);
+	g_fprintf(f,"%s:%d:",watch,g_slist_length(val->head));
+	listpt = g_slist_sort(val->head,(GCompareFunc)cmp_int);
 	
-	while(pt != NULL){
-		g_fprintf(f,"%s",(*pt).data);
-		if((pt = g_slist_next(pt)) != NULL)
+	while(listpt != NULL){
+		g_fprintf(f,"%s",listpt->data);
+		if((listpt = g_slist_next(listpt)) != NULL)
 			g_fprintf(f,",");
 	}
 	g_fprintf(f,"\n");
-}*/
-
-
+}
 fclose(f);
 
-
-/*
-//Data deletion
-for(ind = 0;ind < (*wordlist).len;ind++){
-	node = g_array_index(wordlist,struct word_node,ind);
-	watch = node.word;
-	//g_string_free(watch,TRUE);
-}*/
-
-g_tree_destroy(tree);
+//Finally remove array
+g_array_free(wordlist,TRUE);
+g_hash_table_destroy(table);
 
 //Clear key/data for each allocated
 //Clear linklist
