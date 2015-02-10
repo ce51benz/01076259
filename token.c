75882af722a58736951bb2dcaf28845b002f6626
Suratchanan Kraidech <benchbb01@hotmail.com>
2015-02-11 00:18:56 +0700

    Memory optimization but running time is not beat best time (except for large data)

--- token.c
+++ token.c
@@ -1,6 +1,9 @@
+#include<malloc.h>
+#include<stdlib.h>
 #include<glib.h>
 #include<stdio.h>
 #include<pthread.h>
+#include<string.h>
 GHashTable *table;
 GPtrArray *wordlist;
 
@@ -18,7 +21,7 @@ struct keyval{
 };
 
 struct wordcontainer{
-	gchar *doc_id;
+	char *doc_id;
 	GHashTable *tbl;
 };
 //Compare function use for sorting
@@ -33,20 +36,20 @@ gint cmp_int(gpointer a,gpointer b){
 void *sortlist(void *index){
 long idx = (long)index;
 long count;
-long upper = wordlist->len/4;
-gchar *watch;
+long upper = wordlist->len/12;
+char *watch;
 struct keyval *val;
-if(idx != 0 && idx != 3){
+if(idx != 0 && idx != 11){
 	int uppersum = upper * (idx+1);
-	for(count = (upper*idx);count < uppersum;count++){
-			watch = wordlist->pdata[count];
+	for(count = (upper*idx);count < uppersum;){
+			watch = wordlist->pdata[count++];
 			val = g_hash_table_lookup(table,watch);
 			val->head = g_slist_sort(val->head,(GCompareFunc)cmp_int);
 		}
 	}
-else if(idx == 3){
-	for(count = upper*3;count<wordlist->len;count++){
-		watch = wordlist->pdata[count];
+else if(idx == 11){
+	for(count = upper*11;count<wordlist->len;){
+		watch = wordlist->pdata[count++];
 		val = g_hash_table_lookup(table,watch);
 		val->head = g_slist_sort(val->head,(GCompareFunc)cmp_int);
 	}
@@ -85,20 +88,15 @@ else{
 void *tokenized_word(void *thnum){
 	char ch;
 	struct wordcontainer *wc;
-	gchar *doc_id,*filename;
-	gchar **number;
+	char *doc_id,*filename;
+	char **number;
 	GHashTable *tb;
 	GString *temp = g_string_new(NULL);
 	FILE *f;
-	while(1){
-		pthread_mutex_lock(&dirlc);
-		if((filename = GUINT_TO_POINTER(g_dir_read_name(dir))) == NULL){
-			pthread_mutex_unlock(&dirlc);break;
-		}
-		pthread_mutex_unlock(&dirlc);
+		while((filename = GUINT_TO_POINTER(g_dir_read_name(dir))) != NULL){
 		f = fopen(filename,"r");
 		number = g_strsplit_set(filename,".txt",-1);			
-		doc_id = g_strdup(number[0] + (sizeof(gchar)*4));
+		doc_id = strdup(number[0] + (sizeof(gchar)*4));
 		tb = g_hash_table_new_full((GHashFunc)g_str_hash,(GEqualFunc)g_str_equal,(GDestroyNotify)g_free,NULL);
 		while((ch = fgetc(f)) != EOF){
 			if((ch>64&&ch<91)||(ch>96&&ch<123)){
@@ -107,9 +105,10 @@ void *tokenized_word(void *thnum){
 			else if(temp->len > 0){ 
 				temp = g_string_ascii_down(temp);
 				if(!g_hash_table_contains(tb,temp->str))
-				g_hash_table_add(tb,g_strdup(temp->str));
-				g_string_erase(temp,0,-1);
+				g_hash_table_add(tb,strdup(temp->str));
+				g_string_set_size(temp,0);
 			}
+			
 		}
 		pthread_mutex_lock(&wordtemplc);
 		wc = g_new(struct wordcontainer,1);
@@ -134,7 +133,7 @@ struct wordcontainer *wc;
 gchar **number;
 GHashTableIter *it;
 gpointer watch;
-GString *temp = g_string_new(NULL);
+GString *temp1 = NULL;
 FILE *f;
 	while(1){
 		pthread_mutex_lock(&wordtemplc);
@@ -146,16 +145,16 @@ FILE *f;
 				if(!(val = g_hash_table_lookup(table,watch))){
 					val = g_new(struct keyval,1);
 					val->head = g_slist_prepend(NULL,wc->doc_id);
-					g_hash_table_insert(table,watch,val);				
+					g_hash_table_insert(table,g_strdup(watch),val);
 				}
 				else if(!(val->head->data == wc->doc_id)){
 					val->head = g_slist_prepend(val->head,wc->doc_id);
 				}
 			}
 			//g_hash_table_remove_all(wc->tbl);
-			//g_hash_table_destroy(wc->tbl);
+			g_hash_table_destroy(wc->tbl);
 		}
-		pthread_mutex_unlock(&wordtemplc);	
+		pthread_mutex_unlock(&wordtemplc);
 	}
 }
 
@@ -170,12 +169,12 @@ g_ptr_array_sort(wordlist,(GCompareFunc) cmp_word_node);
 
 }
 int main(int argc,char **argv){
-long oh=0,one=1,two=2,three=3;
+long oh=0,one=1,two=2,three=3,four=4,five=5,six=6,seven=7,eight=8,nine=9,ten=10,eleven=11;
 pthread_mutex_init(&tablelc,NULL);
 pthread_mutex_init(&wordlistlc,NULL);
 pthread_mutex_init(&dirlc,NULL);
 pthread_mutex_init(&wordtemplc,NULL);
-gint ind;
+mallopt(-8,1);
 exitstat=0;
 table = g_hash_table_new((GHashFunc)g_str_hash,(GEqualFunc)g_str_equal);
 dir = g_dir_open(argv[1],0,NULL);
@@ -183,16 +182,22 @@ g_chdir(argv[1]);
 pthread_t t1;
 pthread_t t2;
 pthread_t t3;
+pthread_t t4;
+pthread_t t5;
+pthread_t t6;
+pthread_t t7;
+pthread_t t8;
+pthread_t t9;
+pthread_t t10;
+pthread_t t11;
 pthread_attr_t a1;
-pthread_attr_t a2;
-pthread_attr_t a3;
 pthread_attr_init(&a1);
-pthread_attr_init(&a2);
-pthread_attr_init(&a3);
+//pthread_attr_setschedpolicy(&a1,SCHED_FIFO);
+pthread_attr_setstacksize(&a1,512000);
 wcbox = g_ptr_array_new();
 pthread_create(&t1,&a1,tokenized_word,NULL);
-pthread_create(&t2,&a2,tokenized_word,NULL);
-pthread_create(&t3,&a3,tokenized_word,NULL);
+pthread_create(&t2,&a1,tokenized_word,NULL);
+pthread_create(&t3,&a1,tokenized_word,NULL);
 //Do parallel task
 wordtotable(NULL);
 pthread_join(t1,NULL);
@@ -205,13 +210,28 @@ pthread_create(&t1,&a1,sortlist,(void*)one);
 g_chdir("..");
 ff = fopen("outputtemp1","w");
 g_fprintf(ff,"%d\n",wordlist->len);
-pthread_create(&t2,&a2,sortlist,(void*)two);
-pthread_create(&t3,&a3,sortlist,(void*)three);
-sortlist((void*)oh);
-
+pthread_create(&t2,&a1,sortlist,(void*)two);
+pthread_create(&t3,&a1,sortlist,(void*)three);
+pthread_create(&t4,&a1,sortlist,(void*)four);
+pthread_create(&t5,&a1,sortlist,(void*)five);
+pthread_create(&t6,&a1,sortlist,(void*)six);
+pthread_create(&t7,&a1,sortlist,(void*)seven);
+pthread_create(&t8,&a1,sortlist,(void*)eight);
+pthread_create(&t9,&a1,sortlist,(void*)nine);
+pthread_create(&t10,&a1,sortlist,(void*)ten);
+pthread_create(&t11,&a1,sortlist,(void*)oh);
+sortlist((void*)eleven);
 pthread_join(t1,NULL);
 pthread_join(t2,NULL);
 pthread_join(t3,NULL);
+pthread_join(t4,NULL);
+pthread_join(t5,NULL);
+pthread_join(t6,NULL);
+pthread_join(t7,NULL);
+pthread_join(t8,NULL);
+pthread_join(t9,NULL);
+pthread_join(t10,NULL);
+pthread_join(t11,NULL);
 //Show table data
 
 fclose(ff);
@@ -221,4 +241,3 @@ g_ptr_array_free(wordlist,TRUE);
 g_hash_table_destroy(table);
 return 0;
 }
-
