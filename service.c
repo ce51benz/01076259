23f3414cf562f86f0e473690b8dd5ce3d17bc1fd
Suratchanan Kraidech <benchbb01@hotmail.com>
2015-04-14 03:01:09 +0700

    Assignment 2:Implement the rest of put function - 1
    Update makefile to link math library

--- service.c
+++ service.c
@@ -5,6 +5,9 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <sys/stat.h>
+#include<malloc.h>
+#include<glib.h>
+#include<math.h>
 int ready = 0;
 pthread_mutex_t lock;
 GHashTable *filetable;
@@ -71,6 +74,7 @@ gint numdisk;
 }DISK;
 
 DISK rfosdisk;
+
 
 int strcmpcus(long *str1,long *str2){
 return g_strcmp0(GUINT_TO_POINTER(*str1),GINT_TO_POINTER(*str2));
@@ -182,6 +186,7 @@ void *do_handle_put(void *pa){
 guint err;
 NODE *tail =NULL,*head = NULL;
 guint32 i,blockloc,j,blockwrct=0,nextblock,k;
+FILE_ENT_INMEM *hfentry;FILE_ENT fe;
 getput_param *p = (getput_param*)pa;
 
     if(ready==0)err = EBUSY;
@@ -191,9 +196,8 @@ getput_param *p = (getput_param*)pa;
 	FILE *fp = fopen64(p->path,"r");
 	if(!fp)err = ENOENT;
 	else{
-		if(g_hash_table_contains(filetable,p->key)==FALSE)
+		if((hfentry = g_hash_table_lookup(filetable,p->key)) == NULL)
 		{
-			FILE_ENT fe;
 			for(i=0;i<8;i++)
 				fe.key[i] = p->key[i];
 			struct stat s;
@@ -204,6 +208,7 @@ getput_param *p = (getput_param*)pa;
 			j = ((vblock.bitvec_bl+vblock.file_ent_bl+1)/8)*8;
 			for(i = (vblock.bitvec_bl+vblock.file_ent_bl+1)/8;;i++){
 				for(k=0;k<8;k++){
+					NODE *ptr;
 					if(j>=vblock.numblock || feof(fp))goto freeblkrwchkpt; //0
  					if((bitvec[i] & bytechk[k]) == 0){
 						if(head == NULL){
@@ -215,7 +220,7 @@ getput_param *p = (getput_param*)pa;
 							blockloc = (i*8)+k;
 						}
 						else{
-							NODE *ptr = g_new(NODE,1);
+							ptr = g_new(NODE,1);
 							ptr->data = g_new(DBLOCK,1);
 							(ptr->data)->next = 0;
 							fread((ptr->data)->data,1,28,fp);
@@ -235,13 +240,29 @@ getput_param *p = (getput_param*)pa;
 			fclose(fp);
 			nextblock = fe.sblock = blockloc;NODE *ptr = head;
 			FILE * disk01 = fopen64(rfosdisk.disk1,"rb+");
+			//fseeko64(disk01,nextblock*32,SEEK_CUR);
 			//WRITE DATA TO IMG DISK
+			//GFile *files = g_file_new_for_path(rfosdisk.disk1);
+			//GFileIOStream * fpt = g_file_open_readwrite(files,NULL,NULL);
+			//GOutputStream * stream = g_io_stream_get_output_stream((GIOStream*)fpt);
+			
 			while(ptr != NULL){
 				fseeko64(disk01,nextblock*32,SEEK_SET);
 				fwrite(ptr->data,32,1,disk01);
+				//fseeko64(disk01,((ptr->data)->next-nextblock)*32,SEEK_CUR);
+				nextblock = (ptr->data)->next;
+				ptr = ptr->next;
+			}/*
+			while(ptr != NULL){				
+				g_output_stream_write(stream,ptr->data,32,NULL,NULL);
+				g_seekable_seek((GSeekable*)stream, ((ptr->data)->next-nextblock)*32,G_SEEK_CUR,NULL,NULL);
 				nextblock = (ptr->data)->next;
 				ptr = ptr->next;
 			}
+			g_output_stream_close(stream,NULL,NULL);
+			g_io_stream_close((GIOStream*)fpt,NULL,NULL);
+			g_object_unref(fpt);
+			g_object_unref(files);*/
 			//WRITE FREE BIT VEC TO IMG DISK
 			fseeko64(disk01,32,SEEK_SET);
 			fwrite(bitvec,1,vblock.bitvec_bl*vblock.blocksize,disk01);
@@ -301,7 +322,127 @@ getput_param *p = (getput_param*)pa;
 			fclose(disk01);
 		}
 		else
-		{ g_printf("KEY ALREADY HAVE!\n");/*TO EDIT CASE HTB FOUND*/ }
+		{ 
+			g_printf("KEY ALREADY HAVE!\n");
+			/*TO EDIT CASE HTB FOUND*/ 
+			if(hfentry->valid){
+			//Track what block is already allocated
+			//Check that whether number of block which already allocated is less than required
+			//If yes clear the rest of unused block as unallocated
+			//Else find way to allocate more
+			for(i=0;i<8;i++)
+				fe.key[i] = p->key[i];
+				struct stat s;
+				stat(p->path,&s);
+				fe.size = s.st_size;
+				fe.key[8] = '\0';
+				//Check for avail block.
+				if(ceil(hfentry->size*1.0/vblock.blocksize) == ceil(fe.size*1.0/vblock.blocksize)){
+					//this case can replace data instantly!
+					//no need to update vblock
+					FILE *disk01 = fopen64(rfosdisk.disk1,"rb+");
+					DBLOCK tdata;
+					fseeko64(disk01,hfentry->sblock*vblock.blocksize,SEEK_SET);
+					fread(&tdata,vblock.blocksize,1,disk01);
+					while(TRUE){
+					fread(tdata.data,1,28,fp);
+					fseeko64(disk01,-vblock.blocksize,SEEK_CUR);
+					fwrite(&tdata,vblock.blocksize,1,disk01);
+					if(!tdata.next)break;
+					fseeko64(disk01,tdata.next*vblock.blocksize,SEEK_SET);
+					fread(&tdata,vblock.blocksize,1,disk01);
+					}
+					fclose(fp);
+					//Update fe block..... and write back!
+					fclose(disk01);
+				}				
+				else if(ceil(hfentry->size*1.0/vblock.blocksize) < ceil(fe.size*1.0/vblock.blocksize)){
+					//this case must allocate additional block via find free block...
+					//Use similar procedure of normal case and do it!
+					//Update block for free value
+				}
+				else{
+					//this case must allocate block less than  
+				}
+			}
+			else{
+			//Case file not valid
+			//Do something similar case HTB not found but this time you can write desired file entry to desired location faster. 
+				for(i=0;i<8;i++)
+					fe.key[i] = p->key[i];
+				struct stat s;
+				stat(p->path,&s);
+				fe.size = s.st_size;
+				fe.key[8] = '\0';
+				//Check for avail. block
+				j = ((vblock.bitvec_bl+vblock.file_ent_bl+1)/8)*8;
+				for(i = (vblock.bitvec_bl+vblock.file_ent_bl+1)/8;;i++){
+					for(k=0;k<8;k++){
+						if(j>=vblock.numblock || feof(fp))goto freeblkrwchkpt2; //0
+ 						if((bitvec[i] & bytechk[k]) == 0){
+							if(head == NULL){
+								head = tail = g_new(NODE,1);
+								head->data = g_new(DBLOCK,1);
+								(head->data)->next = 0;
+								fread((head->data)->data,1,28,fp);
+								head->next = NULL;
+								blockloc = (i*8)+k;
+							}
+							else{
+								NODE *ptr = g_new(NODE,1);
+								ptr->data = g_new(DBLOCK,1);
+								(ptr->data)->next = 0;
+								fread((ptr->data)->data,1,28,fp);
+								ptr->next = NULL;
+								(tail->data)->next = (i*8)+k;
+								tail->next = ptr;
+								tail = ptr;			
+							}
+						blockwrct++;
+						bitvec[i] = bitvec[i] | bytechk[k];
+						}
+						j++;
+					}
+				}
+				freeblkrwchkpt2:
+				fclose(fp);
+				nextblock = fe.sblock = blockloc;NODE *ptr = head;
+				FILE * disk01 = fopen64(rfosdisk.disk1,"rb+");
+				while(ptr != NULL){
+					fseeko64(disk01,nextblock*32,SEEK_SET);
+					fwrite(ptr->data,32,1,disk01);
+					nextblock = (ptr->data)->next;
+					ptr = ptr->next;
+				}
+				//WRITE FREE BIT VEC TO IMG DISK
+				fseeko64(disk01,32,SEEK_SET);
+				fwrite(bitvec,1,vblock.bitvec_bl*vblock.blocksize,disk01);
+				fe.valid = 1;
+				fe.atime = time(NULL);
+			
+				vblock.free = vblock.free - blockwrct;
+				vblock.inv_file = vblock.inv_file - 1;
+				
+				g_free(hfentry->key);
+				hfentry->key = g_strdup(fe.key);
+				hfentry->size = fe.size;
+				hfentry->atime = fe.atime;
+				hfentry->sblock = fe.sblock;
+				hfentry->valid = 1;
+	
+				fseeko64(disk01,((vblock.bitvec_bl+1)*32)+(hfentry->fileno*sizeof(FILE_ENT)),SEEK_SET);
+				fwrite(&fe,sizeof(FILE_ENT),1,disk01);
+				fseeko64(disk01,0,SEEK_SET);
+				fwrite(&vblock,32,1,disk01);
+
+				g_printf("KEY => %s\n",fe.key);
+				g_printf("SIZE => %d\n",fe.size);
+				g_printf("ATIME => %ld\n",fe.atime);
+				g_printf("SBLOCK => %d\n",fe.sblock);
+				g_printf("VALID => %d\n",fe.valid);
+				fclose(disk01);
+			}		
+		}
     		err = 0;
 		}
 	}
@@ -310,14 +451,19 @@ rfos_complete_put(p->obj,p->inv,err);
 NODE *ptr = head,*temp;
 /*DO GARBAGE COLLECTION*/
 while(ptr != NULL){
+	//g_free((ptr->data)->data);
 	g_free(ptr->data);
+	//g_object_unref(ptr->data);
+	//g_free(ptr); //
 	temp = ptr;
 	ptr = ptr->next;
 	g_free(temp);
 	}
+
 g_free(p->path);
 g_free(p->key);
 g_free(p);
+g_printf("test454545454\n");
 pthread_exit(0);
 }
 
@@ -619,7 +765,6 @@ rfosdisk.numdisk = argc - 1;
         NULL);
 	filetable = g_hash_table_new(g_str_hash,g_str_equal);
     /* Start the main loop */
-
 //Initialized RFOS (FORMATTING) via another thread
 pthread_create(&test,NULL,dummywork,NULL);
 
